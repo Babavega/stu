36ac241df2fcd1165208e7c992d6fa35a15f4fe5
Babavega <114047064+Babavega@users.noreply.github.com>
2024-06-19 18:55:08 +0530

    Create adsx.js

---
+++ adsx.js
@@ -0,0 +1,19 @@
+let ad1 = "<p>This is an advertisement 1: Your Ad Content Here</p>";
+
+let ad2 = `
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6603511358114879"
+     crossorigin="anonymous"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout-key="-hj-u+l-95+ns"
+     data-ad-client="ca-pub-6603511358114879"
+     data-ad-slot="2104784852"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>`;
+
+let vidad = 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=';
+
+let subNo = "6.4k"
+document.getElementById("subscriber-count").innerText = subNo ;
