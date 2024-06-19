5a2ccc7a5137c20f4695dec15664feef83426338
Babavega <114047064+Babavega@users.noreply.github.com>
2024-06-19 12:34:39 +0530

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,3 +1,22 @@
 let ad1 = "<p>This is an advertisement 1: Your Ad Content Here</p>";
-let ad2 = "<p>This is an advertisement 2: Your Ad Content Here</p>";
+
+let ad2 = `
+<script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"></script>
+<script>
+  window.googletag = window.googletag || {cmd: []};
+  googletag.cmd.push(function() {
+    googletag.defineSlot('/22841960233/Pharmalite.in/Vid-mid', [[300, 75], [468, 60], [320, 100], [320, 50], [300, 100]], 'div-gpt-ad-1718780164545-0').addService(googletag.pubads());
+    googletag.enableServices();
+  });
+</script>
+<!-- /22841960233/Pharmalite.in/Vid-mid -->
+<div id='div-gpt-ad-1718780164545-0' style='min-width: 300px; min-height: 50px;'>
+  <script>
+    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1718780164545-0'); });
+  </script>
+</div>`;
+
 let vidad = 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=';
+
+let subNo = "6.4k"
+document.getElementById("subscriber-count").innerText = subNo ;
