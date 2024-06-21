f32d3095346236ee40680bc4d199191d6d903665
Babavega <114047064+Babavega@users.noreply.github.com>
2024-06-21 12:12:00 +0530

    Update adsf.js

--- adsf.js
+++ adsf.js
@@ -1,14 +1,11 @@
 let ad1 = `ad code1`;
 let ads = [
-    `<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-format="fluid"
-     data-ad-layout-key="-hj-u+l-95+ns"
-     data-ad-client="ca-pub-6603511358114879"
-     data-ad-slot="2104784852"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>`,
+    `<!-- /22841960233/Pharmalite.in -->
+<div id='div-gpt-ad-1718951798497-0' style='min-width: 200px; min-height: 30px;'>
+  <script>
+    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1718951798497-0'); });
+  </script>
+</div>`,
     `<!-- /22841960233/Pharmalite.in/Vid-mid -->
 <div id='div-gpt-ad-1718940186371-0' style='min-width: 300px; min-height: 100px;'>
   <script>
@@ -24,7 +21,12 @@ let ads = [
 <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
 </script>`,
-    'ad code5',
+    `<!-- /22841960233/Adx_Alright -->
+<div id='div-gpt-ad-1718952016052-0' style='min-width: 300px; min-height: 250px;'>
+  <script>
+    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1718952016052-0'); });
+  </script>
+</div>`,
     'ad code6',
     'ad code7',
     'ad code8',
