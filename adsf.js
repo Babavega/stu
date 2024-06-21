26f79f77b9941a6dc5ed8dd2f720bfda243368f4
Babavega <114047064+Babavega@users.noreply.github.com>
2024-06-21 11:49:53 +0530

    Create adsf.js

---
+++ adsf.js
@@ -0,0 +1,22 @@
+let ad1 = ` <!-- /22841960233/Pharmalite.in/Vid-mid -->
+<div id='div-gpt-ad-1718940186371-0' style='min-width: 300px; min-height: 100px;'>
+  <script>
+    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1718940186371-0'); });
+  </script>
+</div>`;
+let ads = [
+    'ad code2',
+    'ad code3',
+    'ad code4',
+    'ad code5',
+    'ad code6',
+    'ad code7',
+    'ad code8',
+    'ad code9',
+    'ad code10',
+];
+
+let vidad = 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator=';
+
+let subNo = "6.4k"
+document.getElementById("subscriber-count").innerText = subNo ;
