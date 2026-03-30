844e2c16c9bfcc47ce421e266d4a377c6feaf121
Babavega <114047064+Babavega@users.noreply.github.com>
2026-03-30 23:51:49 +0530

    Update video ad URL in embed-ads.js

--- embed-ads.js
+++ embed-ads.js
@@ -1,1 +1,1 @@
-let vidad = 'https://candid-twist.com/d_mOF.zadJGONVvxZdG/Uf/GeZm/9RuHZ/UTlYksPQTsYR5pM/TdMgxoM/ziM/tYNWjKkux/MpzOENzrNlyuZ/sZaJWe1_pfdXDz0sxs';
+let vidad = 'https://www.videosprofitnetwork.com/watch.xml?key=739650c9669529b6e9c087501abadcf';
