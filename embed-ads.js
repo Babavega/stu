be51c67d43459e995d22ffe48e61f211b750364f
Babavega <114047064+Babavega@users.noreply.github.com>
2026-03-30 20:11:14 +0530

    Update vidad URL in embed-ads.js

--- embed-ads.js
+++ embed-ads.js
@@ -1,1 +1,1 @@
-let vidad = 'https://candid-twist.com/drm.Fdzhd/GBNLvTZxGwUt/OeXmE9nuJZsUplCkVPdToYy5aM/TPMAx-MrzZMOt/NnjBk_xLM/zyEazbNKwf';
+let vidad = 'https://candid-twist.com/d_mOF.zadJGONVvxZdG/Uf/GeZm/9RuHZ/UTlYksPQTsYR5pM/TdMgxoM/ziM/tYNWjKkux/MpzOENzrNlyuZ/sZaJWe1_pfdXDz0sxs';
