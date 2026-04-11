e7d272513c19bbef62be3d618462a33e2e17cdc1
Babavega <114047064+Babavega@users.noreply.github.com>
2026-04-11 23:33:46 +0530

    Remove unused ad codes from ads.js

--- ads.js
+++ ads.js
@@ -1,17 +1,17 @@
 let ad1 = `ad code1`;
 let ads = [
-    `<!-- /22841960233/Pharmalite.in -->
-<div id='div-gpt-ad-1718951798497-0' style='min-width: 200px; min-height: 30px;'>
-  <script>
-    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1718951798497-0'); });
-  </script>
-</div>`,
-    `<!-- /22841960233/Pharmalite.in/Vid-mid -->
-<div id='div-gpt-ad-1718940186371-0' style='min-width: 300px; min-height: 100px;'>
-  <script>
-    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1718940186371-0'); });
-  </script>
-</div>`,
+    `<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout-key="-gc+3r+68-9q-29"
+     data-ad-client="ca-pub-6603511358114879"
+     data-ad-slot="5495472083"></ins>`,
+    `<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout-key="-gc+3r+68-9q-29"
+     data-ad-client="ca-pub-6603511358114879"
+     data-ad-slot="5495472083"></ins>`,
     `<ins class="adsbygoogle"
      style="display:block"
      data-ad-format="fluid"
@@ -21,12 +21,12 @@ let ads = [
 <script>
      (adsbygoogle = window.adsbygoogle || []).push({});
 </script>`,
-    `<!-- /22841960233/Adx_Alright -->
-<div id='div-gpt-ad-1718952016052-0' style='min-width: 300px; min-height: 250px;'>
-  <script>
-    googletag.cmd.push(function() { googletag.display('div-gpt-ad-1718952016052-0'); });
-  </script>
-</div>`,
+    `<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout-key="-gc+3r+68-9q-29"
+     data-ad-client="ca-pub-6603511358114879"
+     data-ad-slot="5495472083"></ins>`,
     'ad code6',
     'ad code7',
     'ad code8',
